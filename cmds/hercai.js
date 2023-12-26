fc106e5990738a1f994d239812bcceabf8238087
CyniBot <150451220+CyniBot@users.noreply.github.com>
2023-12-26 20:57:10 +0800

    Update hercai.js

--- cmds/hercai.js
+++ cmds/hercai.js
@@ -1,31 +1,30 @@
 const axios = require('axios');
 
 async function hercai(event, api) {
-  const input = event.body.toLowerCase().trim();
+  const input = event.body.toLowerCase().split(' ');
 
   if (input.includes('-help')) {
     const usage = "Usage: hercai [question]\n\n" +
-      "Description: Retrieves a response from the Hercai API based on the provided question.\n\n" +
-      "Example: hercai What is the meaning of life?";
+      "Description: Gets a response from Hercai API based on the provided question.\n\n" +
+      "Example: hercai How does this work?";
     api.sendMessage(usage, event.threadID);
     return;
   }
 
-  const question = encodeURIComponent(input.slice(7));
+  const question = input.slice(1).join(' ');
 
   try {
-    const apiUrl = `https://hercai.onrender.com/v3-beta/hercai?question=${question}`;
-    const response = await axios.get(apiUrl);
-    const answer = response.data;
+    const response = await axios.get(`https://hercai.onrender.com/v3-beta/hercai?question=${encodeURIComponent(question)}`);
+    const hercaiResponse = response.data;
 
-    if (answer) {
-      api.sendMessage(answer, event.threadID);
+    if (hercaiResponse && hercaiResponse.reply) {
+      api.sendMessage(hercaiResponse.reply, event.threadID);
     } else {
-      api.sendMessage('Unable to fetch a response from Hercai API at the moment. Please try again later.', event.threadID);
+      api.sendMessage('Unable to get a response from Hercai at the moment. Please try again later.', event.threadID);
     }
   } catch (err) {
-    console.error(`Error fetching response from Hercai API: ${err}`);
-    api.sendMessage('Failed to fetch a response. Please try again later.', event.threadID);
+    console.error(`Error fetching Hercai response: ${err}`);
+    api.sendMessage('Failed to get a response from Hercai. Please try again later.', event.threadID);
   }
 }
 
